2c464247d686d0053a5a81c5183ac31f95778ffe
mohammad norouzi <smileenginesoft@gmail.com>
2022-06-20 17:57:59 +0430

    ...

---
+++ server_go/main.go
@@ -0,0 +1,37 @@
+package main
+ 
+import (
+    "log"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+)
+ 
+// NewProxy takes target host and creates a reverse proxy
+func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
+    url, err := url.Parse(targetHost)
+    if err != nil {
+        return nil, err
+    }
+ 
+    return httputil.NewSingleHostReverseProxy(url), nil
+}
+ 
+// ProxyRequestHandler handles the http request using proxy
+func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+    return func(w http.ResponseWriter, r *http.Request) {
+        proxy.ServeHTTP(w, r)
+    }
+}
+ 
+func main() {
+    // initialize a reverse proxy and pass the actual backend server url here
+    proxy, err := NewProxy("http://my-api-server.com")
+    if err != nil {
+        panic(err)
+    }
+ 
+    // handle all requests to your server using the proxy
+    http.HandleFunc("/", ProxyRequestHandler(proxy))
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
